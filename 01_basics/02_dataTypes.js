82aa80947620dbb57e538affad68584f77495829
Tanweer567 <69241289+Tanweer567@users.noreply.github.com>
2023-08-19 10:43:33 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are usingh nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+    console.log("Tanweer");
+
+    let name = "Tanweer"
+    let age = 18
+    let isLoggedIn = false
+    let state;
+
+    // number =>2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+
+
+    // object
+
+    console.log(typeof "Tanweer");
+
+    console.log(typeof undefine); // undefined
+    console.log(typeof null); // object
