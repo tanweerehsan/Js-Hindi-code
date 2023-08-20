ecda4fee50689c2ea6a8a44f522f6040945c5aeb
Tanweer567 <69241289+Tanweer567@users.noreply.github.com>
2023-08-20 14:49:24 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,6 +20,48 @@ let isLoggedIn = ""
 //"Tanweer" => true
 
 let someNumber = 33
+
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//****************************** Operation ******************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = "tanweer"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+//console.log( (3+4)*5%3);
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
+
+num1= num2= num3=2+2
+
+
