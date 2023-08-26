72fc4d7d11e6e9dd176fe0ab0dd4433a883e7e82
Tanweer567 <69241289+Tanweer567@users.noreply.github.com>
2023-08-26 09:41:33 +0000

    variable

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,12 @@
+// 'const' ka use  wha jaha value mei koi chnges nhi hota hai . or 'let' ka use wha jaha value chnge ho skta hai.
+
 const accountId = 14453
 let accountEmail = "Tan@gmail.com"
 var accountPassword = "12345"
 accountCity = "Asansol"
 let accountState;
 
-//accountId = 2 // not allowed
+//accountId = 2 // after running code 'const'not allowed to chnge the value.
 
 accountEmail = "hc@hc.com"
 accountPassword = "212121"
@@ -12,9 +14,9 @@ accountCity = "Burnpur"
 
 console.log(accountId);
 /*
-Prefer not to user var
+Prefer not to user 'var'
 beacuse of issue in block scope amd functional scope
 
 */
-
-console.table([accountId, accountEmail, accountPassword, accountCity])
+// ek sath sbhi ko console me run karne value ko paane ke liye 'console.table' k use krnege.
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
