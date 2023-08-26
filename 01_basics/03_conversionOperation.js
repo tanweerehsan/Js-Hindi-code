61a60273e8d1af4d2665a42adf84a1f6d7f537c4
Tanweer567 <69241289+Tanweer567@users.noreply.github.com>
2023-08-26 10:08:00 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -5,8 +5,8 @@ let score = "Tanweer"
 
 let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
-//console.log(valueInNumber);
-
+//console.log(valueInNumber); (for checking what value gave me)
+//  these are the results of which convert
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0;
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = ""
 //let booleanIsLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn);
-
+//  these are the also result
 // 1 => true; 0 => false;
 // "" => false
 //"Tanweer" => true
@@ -40,17 +40,17 @@ console.log(negValue);
 //console.log(2%3);
 
 let str1 = "hello"
-let str2 = "tanweer"
+let str2 = " tanweer"
 
 let str3 = str1 + str2
 console.log(str3);
 
 
-//console.log("1"+2);
-//console.log(1+"2");
-//console.log("1"+2+2);
-//console.log(1+2+"2");
-//console.log( (3+4)*5%3);
+//console.log("1"+2); their result = 12
+//console.log(1+"2"); their result = 12
+//console.log("1"+2+2); their result = 122
+//console.log(1+2+"2"); their result = 32 (jab last m string hoga to phle 2 number ko add krnga and last m string ke sath put together krdnga)
+//console.log( (3 + 4)* 5 % 3);
 
 //console.log(+true);
 //console.log(+"");
@@ -61,7 +61,6 @@ let gameCounter =100
 gameCounter++;
 console.log(gameCounter);
 
-
-num1= num2= num3=2+2
+// num1= num2= num3=2+2 (don't do like this cz readabilty z not gud)
 
 
