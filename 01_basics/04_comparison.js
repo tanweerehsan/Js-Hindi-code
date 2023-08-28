e6677f2bf84f6e5f8720b5679836df8a6dd2038d
Tanweer567 <69241289+Tanweer567@users.noreply.github.com>
2023-08-28 09:50:42 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// line no. 11/12/13 ka answer false/false/true aayenga .
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// isme teeno ka answer same hoga false
+
+// null aur undefined ke conversion ko mostly tym avoid karna chahiye qk ye kch v ans provide . isme kch confirm nh h
+
+// === (strict equality)
+
+// console.log("2" === 2);
+// === ye srtictly check krta hai .
