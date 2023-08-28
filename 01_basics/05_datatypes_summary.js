f941b4f00785f2f782e9e733f7d34773c539c5c6
Tanweer567 <69241289+Tanweer567@users.noreply.github.com>
2023-08-28 10:20:37 +0000

    datatypes summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,34 @@
+// premitive
+
+// 7 types : String, Number, Boolean, Null, undefined, Bigint, Symbol
+
+const score =100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+// const bigNumber = 669542125855n
+
+
+// Reference (Non Premitive)
+
+//  Array, Objects, Function
+
+const heros = ["shaktiman", "naagraj","doga"]
+let myObj= {
+    name: "tanweer",
+    age: 22,
+}
+
+const myfunction = function (){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
