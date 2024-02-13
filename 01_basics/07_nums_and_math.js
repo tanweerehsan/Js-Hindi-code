531b65bf9ce2539b242c108717784cd1c8d7c621
Tanweer Ehsan <69241289+tanweerehsan@users.noreply.github.com>
2024-02-13 07:07:09 +0000

    main

--- 01_basics/07_nums_and_math.js
+++ 01_basics/07_nums_and_math.js
@@ -1,17 +1,21 @@
-const score = 400
+const score = 400;
+
 // console.log(score);
 
-const balance = new Number (1000)
+const balance = new Number (1000);
+
 // console.log(balance);
 
 // console.log(balance.toString().length);
+
 // console.log(balance.toFixed(2));
 
-const otherNumber = 23.8996
+const otherNumber = 23.8996;
 
 // console.log(otherNumber.toPrecision(3));
 
-const hundreds = 1000000
+const hundreds = 1000000;
+
 // console.log(hundreds.toLocaleString('en-IN'));
 
 // *********************Maths**********************
@@ -27,6 +31,7 @@ const hundreds = 1000000
 console.log(Math.random());
 
 console.log((Math.random()*10) + 1);
+
 console.log(Math.floor(Math.random()*10) + 1);
 
 const max = 20
